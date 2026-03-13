941c4d03ff9659d398a37c0f43d7567bbcdc77c2
Lucy <lucy.bloemen@student.howest.be>
2026-03-13 14:58:36 +0100

    6th added init of widget and browse folder function

--- Assignment2_texture_packing/packing_textures.py
+++ Assignment2_texture_packing/packing_textures.py
@@ -22,4 +22,28 @@ class TexturePacker(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.ui = load_ui("resources/main_window.ui", self)
+        # load ui
+        self.ui = load_ui("Resources/main_window.ui", self)
+        self.setCentralWidget(self.ui)
+        self.setWindowTitle("Texture Packer")
+
+        # init all parts of widget
+        self.btn_browse_folder: QPushButton = self.btn_browse_folder
+        self.btn_browse_save: QPushButton = self.btn_browse_save
+        self.btn_apply: QPushButton = self.btn_apply
+        self.btn_cancel: QPushButton = self.btn_cancel
+        self.line_texture_path: QLineEdit = self.line_texture_path
+        self.line_save_path: QLineEdit = self.line_save_path
+        self.text_texture_files: QPlainTextEdit = self.text_texture_files
+        self.text_texture_made: QPlainTextEdit = self.text_texture_made
+
+        # connect to function
+        self.btn_browse_folder.clicked.connect(self.browse_folder("texture", QLineEdit(self.btn_browse_folder)))
+        self.btn_browse_save.clicked.connect(self.browse_folder("save", QLineEdit(self.btn_browse_save)))
+
+    def browse_folder(self, selection_name: str, path: QLineEdit):
+
+        folder = QFileDialog.getExistingDirectory(self, f"Select {selection_name} Directory")
+        if folder:
+            path.setText(folder)
+        config_manager.save_config({f"last_{selection_name}_path": folder})
